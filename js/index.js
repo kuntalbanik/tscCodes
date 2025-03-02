5201888545c8c23f14c053efba9c27a420e89690
Kuntal Banik <deisiduke1986@gmail.com>
2025-03-02 23:17:18 +0530

    code updated

---
+++ js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let a1 = 20;
+a1 = 10;
+function add(a, b) {
+    return a + b;
+}
+const x = add(1, a1);
+console.log(x);
+//# sourceMappingURL=index.js.map
---
+++ ts/index.ts
@@ -0,0 +1,14 @@
+// let a1: number = 20;
+// a1 = 10;
+
+// function add(a: number, b: number): number {
+//   return a + b; // return the addition
+// }
+
+// const x = add(1,a1);
+// console.log(x);
+
+
+function testAnyType(val: string): string{
+  return val
+}
